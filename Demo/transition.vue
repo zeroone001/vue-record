87bdc0aaa2158adb261794e35ab86677d7d2515f
zeroone001 <ilylys@163.com>
2020-01-15 17:01:08 +0800

    update

--- Demo/transition.vue
+++ Demo/transition.vue
@@ -3,8 +3,16 @@
     <div @click="show = !show">
         按钮
     </div>
-    <transition name="aa">
+    <div @click="showbb = !showbb">
+        按钮2
+    </div>
+    <!-- <transition name="aa">
         <div v-if="show">test</div>
+    </transition> -->
+    <transition 
+        name="bounce"
+        @enter="enter" >
+        <div v-if="showbb">hello</div>
     </transition>
 </div>
 </template>
@@ -12,15 +20,39 @@
 export default {
     data () {
         return {
-            show: false
+            show: false,
+            showbb: true,
+            el: ''
         };
     },
     methods: {
-
+        enter (el, done) {
+            done();
+        }
     }
 }
 </script>
 <style>
+div{
+    text-align: center;
+}
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
 .aa-enter, .aa-leave-to {
     opacity: 0;
 }
